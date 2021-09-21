efbe83ac29d3e0a6c99577bccf75b2d72b660c77
Michael Kamau <tokamau@gmail.com>
2021-09-21 20:25:36 +0300

    Add placeholder impl to request for location permissions

--- app/src/main/java/com/mikekamau/geofences/ui/DashboardFragment.kt
+++ app/src/main/java/com/mikekamau/geofences/ui/DashboardFragment.kt
@@ -1,9 +1,18 @@
 package com.mikekamau.geofences.ui
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -13,7 +22,24 @@ import com.mikekamau.geofences.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
-  val geofencesViewModel: GeofenceViewModel by activityViewModels {
+  // Register the permissions callback, which handles the user's response to the
+  // system permissions dialog.
+  private val requestPermissionsLauncher: ActivityResultLauncher<Array<String>> =
+    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+    { permissions ->
+      permissions.entries.forEach {
+        if (it.value) {
+          // this permission is granted
+          Log.d(TAG, "${it.key} is granted")
+        } else {
+          // this permission is not granted
+          Log.d(TAG, "${it.key} is not granted")
+          RequestPermissionDialog(null).show(requireActivity().supportFragmentManager, "dialog")
+        }
+      }
+    }
+
+  private val geofencesViewModel: GeofenceViewModel by activityViewModels {
     GeofenceViewModelFactory(GeofencesApplication.getInstance().repository)
   }
 
@@ -34,6 +60,7 @@ class DashboardFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
+
     geofencesViewModel.allGeoFences.observe(viewLifecycleOwner) { geofences ->
       if (geofences.isEmpty()) {
         binding.tvWarningNoGeofence.visibility = View.VISIBLE
@@ -43,11 +70,74 @@ class DashboardFragment : Fragment() {
     }
 
     binding.btnAddGeofence.setOnClickListener {
-      navigateToMaps()
+      navigateWithPermissionCheck()
     }
   }
 
-  fun navigateToMaps() {
+  private fun navigateWithPermissionCheck() {
+    when {
+      ContextCompat.checkSelfPermission(
+        requireActivity(),
+        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+      ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+        requireActivity(),
+        Manifest.permission.ACCESS_FINE_LOCATION
+      ) == PackageManager.PERMISSION_GRANTED -> {
+        navigateToMaps()
+      }
+
+      shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION) -> {
+        // show dialog explaining need for this permission
+        RequestPermissionDialog(requestPermissionsLauncher).show(
+          requireActivity().supportFragmentManager,
+          "dialog"
+        )
+      }
+
+      shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+        // show dialog explaining need for this permission
+        RequestPermissionDialog(requestPermissionsLauncher).show(
+          requireActivity().supportFragmentManager,
+          "dialog"
+        )
+      }
+
+      else -> {
+        requestPermissionsLauncher.launch(
+          arrayOf(
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+          )
+        )
+      }
+
+    }
+  }
+
+  /**
+   * This dialog explains to the user why we need access to Location permissions
+   */
+  class RequestPermissionDialog(val permissionLauncher: ActivityResultLauncher<Array<String>>?) :
+    DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+      return AlertDialog.Builder(activity)
+        .setMessage(R.string.label_permission_request)
+        .setPositiveButton(android.R.string.ok) { dialog, which ->
+          // Do not finish the Activity while requesting permission.
+          permissionLauncher?.launch(
+            arrayOf(
+              Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+              Manifest.permission.ACCESS_FINE_LOCATION
+            )
+          )
+        }
+        .setNegativeButton(android.R.string.cancel, null)
+        .create()
+    }
+  }
+
+  private fun navigateToMaps() {
     findNavController().navigate(R.id.dest_maps, null)
   }
 
