5965a268bca4fe1bf69ddbc22458f807aca4b650
Michael Kamau <tokamau@gmail.com>
2021-09-27 12:09:59 +0300

    Add permission check before launch map fragment

--- app/src/main/java/com/mikekamau/geofences/ui/DashboardFragment.kt
+++ app/src/main/java/com/mikekamau/geofences/ui/DashboardFragment.kt
@@ -1,18 +1,9 @@
 package com.mikekamau.geofences.ui
 
-import android.Manifest
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -21,23 +12,6 @@ import com.mikekamau.geofences.R
 import com.mikekamau.geofences.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
-
-  // Register the permissions callback, which handles the user's response to the
-  // system permissions dialog.
-  private val requestPermissionsLauncher: ActivityResultLauncher<Array<String>> =
-    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
-    { permissions ->
-      permissions.entries.forEach {
-        if (it.value) {
-          // this permission is granted
-          Log.d(TAG, "${it.key} is granted")
-        } else {
-          // this permission is not granted
-          Log.d(TAG, "${it.key} is not granted")
-          RequestPermissionDialog(null).show(requireActivity().supportFragmentManager, "dialog")
-        }
-      }
-    }
 
   private val geofencesViewModel: GeofenceViewModel by activityViewModels {
     GeofenceViewModelFactory(GeofencesApplication.getInstance().repository)
@@ -70,75 +44,13 @@ class DashboardFragment : Fragment() {
     }
 
     binding.btnAddGeofence.setOnClickListener {
-      navigateWithPermissionCheck()
+      navigateToPermissionFrag()
     }
   }
 
-  private fun navigateWithPermissionCheck() {
-    when {
-      ContextCompat.checkSelfPermission(
-        requireActivity(),
-        Manifest.permission.ACCESS_BACKGROUND_LOCATION
-      ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-        requireActivity(),
-        Manifest.permission.ACCESS_FINE_LOCATION
-      ) == PackageManager.PERMISSION_GRANTED -> {
-        navigateToMaps()
-      }
 
-      shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION) -> {
-        // show dialog explaining need for this permission
-        RequestPermissionDialog(requestPermissionsLauncher).show(
-          requireActivity().supportFragmentManager,
-          "dialog"
-        )
-      }
-
-      shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-        // show dialog explaining need for this permission
-        RequestPermissionDialog(requestPermissionsLauncher).show(
-          requireActivity().supportFragmentManager,
-          "dialog"
-        )
-      }
-
-      else -> {
-        requestPermissionsLauncher.launch(
-          arrayOf(
-            Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
-          )
-        )
-      }
-
-    }
-  }
-
-  /**
-   * This dialog explains to the user why we need access to Location permissions
-   */
-  class RequestPermissionDialog(val permissionLauncher: ActivityResultLauncher<Array<String>>?) :
-    DialogFragment() {
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-      return AlertDialog.Builder(activity)
-        .setMessage(R.string.label_permission_request)
-        .setPositiveButton(android.R.string.ok) { dialog, which ->
-          // Do not finish the Activity while requesting permission.
-          permissionLauncher?.launch(
-            arrayOf(
-              Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-              Manifest.permission.ACCESS_FINE_LOCATION
-            )
-          )
-        }
-        .setNegativeButton(android.R.string.cancel, null)
-        .create()
-    }
-  }
-
-  private fun navigateToMaps() {
-    findNavController().navigate(R.id.dest_maps, null)
+  private fun navigateToPermissionFrag() {
+    findNavController().navigate(R.id.dest_permissionFragment, null)
   }
 
   companion object {
--- app/src/main/java/com/mikekamau/geofences/ui/MapsFragment.kt
+++ app/src/main/java/com/mikekamau/geofences/ui/MapsFragment.kt
@@ -1,21 +1,34 @@
 package com.mikekamau.geofences.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.mikekamau.geofences.R
+import com.mikekamau.geofences.data.GeofenceUtils
 import com.mikekamau.geofences.databinding.FragmentMapsBinding
 
 class MapsFragment : Fragment() {
 
   lateinit var binding: FragmentMapsBinding
+  private lateinit var map: GoogleMap
+  private lateinit var geofencingClient: GeofencingClient
+  private lateinit var geofenceUtils: GeofenceUtils
+
   private val onMapReadyCallback = OnMapReadyCallback { googleMap ->
     /**
      * Manipulates the map once available.
@@ -27,18 +40,22 @@ class MapsFragment : Fragment() {
      * user has installed Google Play services and returned to the app.
      */
 
-    // update UI settings
-    with(googleMap.uiSettings) {
-      isCompassEnabled = true
-    }
+    map = googleMap
+    map.setOnMapLongClickListener(onMapLongClickListener)
 
-    val home = LatLng(-1.2811374954430768, 36.66097762870249)
-    googleMap.addMarker(MarkerOptions().position(home).title("Home"))
-    googleMap.animateCamera(
+    val home = getUserCurrentLocation()
+    map.addMarker(MarkerOptions().position(home).title("Home"))
+    map.animateCamera(
       CameraUpdateFactory.newLatLngZoom(home, 17.5f),
       500, null
     )
   }
+
+  private val onMapLongClickListener = GoogleMap.OnMapLongClickListener { position ->
+    addMarker(position)
+    addCircle(position, 100.0)
+  }
+
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -53,10 +70,37 @@ class MapsFragment : Fragment() {
     super.onViewCreated(view, savedInstanceState)
     val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
     mapFragment?.getMapAsync(onMapReadyCallback)
+    geofencingClient = LocationServices.getGeofencingClient(requireActivity())
+    geofenceUtils = GeofenceUtils(requireActivity())
 
     binding.btnContinue.setOnClickListener {
       AddGeofenceFragment.newInstance().show(childFragmentManager, AddGeofenceFragment.TAG)
     }
 
   }
+
+
+  private fun addMarker(position: LatLng) {
+    map.clear()
+    map.addMarker(MarkerOptions().apply {
+      position(position)
+      draggable(false)
+    })
+  }
+
+  private fun addCircle(position: LatLng, radiusMeters: Double) {
+    map.addCircle(CircleOptions().apply {
+      center(position)
+      radius(radiusMeters)
+      strokeWidth(4f)
+      strokeColor(Color.RED)
+      fillColor(Color.CYAN)
+    })
+  }
+
+  private fun getUserCurrentLocation(): LatLng {
+    // todo: add proper impl
+    return LatLng(-1.2811374954430768, 36.66097762870249);
+  }
+
 }
